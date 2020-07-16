4d5b0c212748a93dd451197cc9f48408014dccfb
Geetansh <gitansh49@gmail.com>
2020-07-16 23:11:00 +0530

    initial commit

---
+++ build/precache-manifest.d053e4689242d9f01e62788c800f1384.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b590dbde441c25ff9e102d128ef989d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "81c60b3770546a60310e",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "82dde07299bcbe0e49cf",
+    "url": "/static/css/main.0e758b8a.chunk.css"
+  },
+  {
+    "revision": "81c60b3770546a60310e",
+    "url": "/static/js/2.ce3203d5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ce3203d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82dde07299bcbe0e49cf",
+    "url": "/static/js/main.fb006b73.chunk.js"
+  },
+  {
+    "revision": "fdb66bf0534857f19626",
+    "url": "/static/js/runtime-main.8f3ae637.js"
+  }
+]);
